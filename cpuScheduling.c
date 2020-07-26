cf5a80590b2ee3b3ed2d91bf204a81dede340ce8
jatin jain <jatinvatsalya@gmail.com>
2020-07-27 00:40:10 +0530

    Update cpuScheduling.c

--- cpuScheduling.c
+++ cpuScheduling.c
@@ -19,14 +19,14 @@ struct PCB handle_process_arrival_pp(
 
     if ( isNullPcb(current_process) )
     {
-        memcpy( &result, &new_process, sizeof( result ) );
+        memcpy( &result, &new_process, sizeof( struct PCB ) );
         result.execution_starttime = timestamp;
         result.execution_endtime = timestamp + result.total_bursttime;
         result.remaining_bursttime = result.total_bursttime;
     }
     else if ( new_process.process_priority >= current_process.process_priority )
     {
-        memcpy( &result, &current_process, sizeof( result ) );
+        memcpy( &result, &current_process, sizeof( struct PCB ) );
         iAdded = addRequest( ready_queue, queue_cnt, new_process );
         ready_queue[iAdded].execution_starttime = 0;
         ready_queue[iAdded].execution_endtime = 0;
@@ -36,13 +36,51 @@ struct PCB handle_process_arrival_pp(
     else
     {
         iAdded = addRequest( ready_queue, queue_cnt, new_process );
-        memcpy( &result, &new_process, sizeof( result ) );
+        memcpy( &result, &new_process, sizeof( struct PCB ) );
         result.execution_starttime = timestamp;
         result.execution_endtime = timestamp + result.total_bursttime;
         result.remaining_bursttime = result.total_bursttime;
         ready_queue[iAdded].execution_endtime = 0;
         ready_queue[iAdded].remaining_bursttime = ready_queue[iAdded].total_bursttime;
     }
+
+    return result;
+}
+
+struct PCB handle_process_completion_pp(
+    struct PCB ready_queue[QUEUEMAX],
+    int *queue_cnt,
+    int timestamp)
+{
+    int i;
+    int iLow;
+    int lowestPriority;
+    int initialized = 0;
+    struct PCB result = {0, 0, 0, 0, 0, 0, 0};
+
+    if ( *queue_cnt )
+    {
+        for ( i = 0; i < QUEUEMAX; ++i )
+        {
+            if ( ! initialized )
+            {
+                lowestPriority = ready_queue[ i ].process_priority;
+                initialized = 1;
+                iLow = i;
+            }
+            if ( ready_queue[ i ].process_priority < lowestPriority )
+            {
+                lowestPriority = ready_queue[ i ].process_priority;
+                iLow = i;
+            }
+        }
+        memcpy( &result, &ready_queue[ iLow ], sizeof(struct PCB) );
+        memset( &ready_queue[ iLow ], 0, sizeof( struct PCB ) );
+        result.execution_starttime = timestamp;
+        result.execution_endtime = timestamp + result.remaining_bursttime;
+    }
+
+    return result;
 }
 
 static int addRequest(  //we need to clear that if queue is full then what to do.
